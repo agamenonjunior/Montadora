fcb13991ebc1f4c7e144be1b06b129c08cc2018d
agamenon <agamenon_junior1@hotmail.com>
2025-12-15 16:25:04 -0300

    feat: Implementação da Classe HondaHRV

---
+++ src/main/java/io/github/agamenonjunior/montadora/montadora/HondaHRV.java
@@ -0,0 +1,14 @@
+package io.github.agamenonjunior.montadora.montadora;
+
+import java.awt.*;
+
+public class HondaHRV extends Carro  {
+
+
+    public HondaHRV(Motor motor) {
+        super(motor);
+        setModelo("HRV");
+        setCor(Color.BLACK);
+        setMontadora(Montadora.HONDA);
+    }
+}
